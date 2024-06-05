bb8c8c66a58b75f581a67e287ff5a43ac01f2961
techops-chirag <chirag111saini@gmail.com>
2024-06-05 12:50:42 +0530

    fourth commit

--- src/firebase-Config.js
+++ src/firebase-Config.js
@@ -1,9 +1,9 @@
 import  firebase from "firebase";
 var firebaseConfig = {
     apiKey: "AIzaSyDedtmNx-cfWiyleVcb9Sm9x8R77fGUZoo",
-    authDomain: "whatsapp-clone-rishav.firebaseapp.com",
-    projectId: "whatsapp-clone-rishav",
-    storageBucket: "whatsapp-clone-rishav.appspot.com",
+    authDomain: "whatsapp-clone.firebaseapp.com",
+    projectId: "whatsapp-clone",
+    storageBucket: "whatsapp-clone.appspot.com",
     messagingSenderId: "902572169035",
     appId: "1:902572169035:web:a53034dd4cbfb3a6dc7ace",
     measurementId: "G-87WVDGQBKE"
